03201753a501a46ff140f52395c6092b15ee5df0
Matthew Barrett <mb2884@alumni.princeton.edu>
2025-07-09 03:06:30 -0700

    uibuild

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -5,8 +5,8 @@ const Person = require("./models/person");
 
 const app = express();
 
-app.use(express.json());
 app.use(express.static("dist"));
+app.use(express.json());
 
 //app.use(morgan("tiny"));
 
@@ -40,34 +40,31 @@ let persons = [
   },
 ];
 
-// GET all persons
 app.get("/api/persons", (request, response) => {
   Person.find({}).then(persons => {
     response.json(persons);
   });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const person = persons.find(person => person.id === id);
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      person ? response.json(person) : response.status(404).end();
+    })
+    .catch(error => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  persons = persons.filter(person => person.id !== id);
-
-  response.status(204).end();
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end();
+    })
+    .catch(error => next(error));
 });
 
 app.get("/info", (request, response) => {
-  response.send(
-    `<p>Phonebook has info for ${persons.length} people</p>${new Date()}`
+  Person.countDocuments({}).then(count =>
+    response.send(`<p>Phonebook has info for ${count} people</p>${new Date()}`)
   );
 });
 
@@ -85,22 +82,50 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
-  if (persons.some(person => person.name === body.name)) {
-    return response.status(400).json({
-      error: "name must be unique",
-    });
-  }
+  // if (persons.some(person => person.name === body.name)) {
+  //   return response.status(400).json({
+  //     error: "name must be unique",
+  //   });
+  // }
 
-  const person = {
-    id: String(Math.floor(Math.random() * 100000)),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  };
+  });
 
-  persons = persons.concat(person);
-
-  response.json(person);
+  person.save().then(savedPerson => {
+    response.json(savedPerson);
+  });
 });
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const { name, number } = request.body;
+
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person) {
+        return response.status(404).end();
+      }
+
+      person.name = name;
+      person.number = number;
+
+      return person.save().then(updatedPerson => response.json(updatedPerson));
+    })
+    .catch(error => next(error));
+});
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
